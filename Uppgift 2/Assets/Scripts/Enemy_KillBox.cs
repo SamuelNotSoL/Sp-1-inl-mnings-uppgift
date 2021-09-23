f20f4e09c59a6b6cbdbd07e6389b4ffa634ac0c5
SamuelNotSoL <saro2753@student.su.se>
2021-09-23 13:49:52 +0200

    Enemy Sound Added
    Enemies gör nu ljud ifrån sig när dem dör. Ljudvolymen på alla levels är nu på 0.2 så att man kan höra allt annat.

--- Uppgift 2/Assets/Scripts/Enemy_KillBox.cs
+++ Uppgift 2/Assets/Scripts/Enemy_KillBox.cs
@@ -6,6 +6,8 @@ public class Enemy_KillBox : MonoBehaviour
 {
 
     GameObject gameobjectToKill;
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip soundOfDeathClip;
 
     private void Start() {
 
@@ -18,6 +20,7 @@ public class Enemy_KillBox : MonoBehaviour
         if(collision.CompareTag("Player") == true) {
             if (collision.gameObject.GetComponent<PlayerMovement>().isFalling() == true) {
                 gameObject.GetComponentInParent<Enemy_SpiderMovement>().killMe();
+                audioSource.PlayOneShot(soundOfDeathClip);
             }
         }
     }
